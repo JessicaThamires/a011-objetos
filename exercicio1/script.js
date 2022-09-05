9238dc1c857220da70e1552dfb11d48aec17a101
Jessica <jessica.thamiresnunes@gmail.com>
2022-09-05 19:58:28 -0300

    Exercícios relizados com sucesso

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,14 +1,4 @@
-// # Exercício 1
 
-// Crie um array vazio chamado `sacolao`, e siga os passos abaixo:
-
-// a) Crie três objetos que vão representar frutas do sacolão. Os objetos
-// devem ter as seguintes propriedades: 
-// ```jsx
-// nome: string;
-// preco: number;
-// disponibilidade: boolean;
-// ```
 
 const sacolao =[]
 
